7c2d112d06ced412244db3f39568ce3f24241f36
Nishit Parekh <ug201211022@iitj.ac.in>
2014-10-16 18:30:03 +0530

    Added Elevators

--- elevator.py
+++ elevator.py
@@ -1,17 +1,16 @@
 class Elevator(object):
 
+
 	def __init__(self,canvas,name):
 
 		self.name = name
-		self.body = canvas.create_rectangle(200,200,200,200,fill="#000")
+		self.body = canvas.create_rectangle(50+(self.name*50),50,80+(self.name*50),150,fill="#060")
 		self.x = canvas.coords(self.body)[0]
 		self.y = canvas.coords(self.body)[1]
 
 		self.floor_list = []
-		for i in range(0,11):
-			floor_list[i] = False
-
-		print len(floor_list)
+		for i in range(0,10):
+			self.floor_list.append(False)
 
 		self.call_queue = []
 
@@ -19,4 +18,11 @@ class Elevator(object):
 		self.move_status = "idle"
 		self.current_floor = 0
 
-		self.vel = 0
+		self.vel = 5
+
+	def update(self,canvas):
+
+		canvas.move(self.body,0,self.vel)
+		canvas.update()
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from elevator import *
 from Tkinter import *
 
 class Application:    
@@ -6,16 +7,34 @@ class Application:
 
         global building
 
+        self.master = master
         self.building = Canvas(master, width = 1000, height = 700)
         self.building.pack(side=LEFT)
 
         self.panel = Canvas(master, width = 300, height = 700)
-        self.panel.pack(side=LEFT)        
-        
+        self.panel.pack(side=LEFT)
+
+        self.make_elevators(self.building)
+
+    def make_elevators(self,canvas):
+
+        self.elevator_list = []
+        for i in range(0,4):
+            e = Elevator(canvas, i)
+            self.elevator_list.append(e)
+
+    def simulate(self):
+
+        for e in self.elevator_list:
+            e.update(self.building)
+
+        self.master.after(40,self.simulate)
         
 master = Tk()
 app = Application(master)
+app.simulate()
 master.mainloop()
+
 
 
 
